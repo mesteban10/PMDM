632be75d6ad1ebb456461e1a2d72f127c3acb2f5
mesteban10 <mariaesteban1997@gmail.com>
2021-10-08 19:21:45 +0200

    Mismo ejercicio que el 1, pero usando relativeLayout

---
+++ app/src/main/java/com/mestabn/myapplication/ut3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mestabn.myapplication.ut3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mestabn.myapplication.R
+
+class UiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ui)
+    }
+}
