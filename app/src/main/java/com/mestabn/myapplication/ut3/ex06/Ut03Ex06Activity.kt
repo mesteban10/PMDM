910c7da3bcac1b8158be570bc259afa399074de4
mesteban10 <mariaesteban1997@gmail.com>
2021-11-24 18:39:05 +0100

    Ejercico que implementa fragments

---
+++ app/src/main/java/com/mestabn/myapplication/ut3/ex06/Ut03Ex06Activity.kt
@@ -0,0 +1,11 @@
+package com.mestabn.myapplication.ut3.ex06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ut03Ex06Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut03_ex06)
+    }
+}
