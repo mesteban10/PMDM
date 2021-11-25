251f00401afad2957ae46a33f3b1efbdc89daa28
mesteban10 <mariaesteban1997@gmail.com>
2021-11-25 13:02:13 +0100

    Se incluye la toolbar en el main de ambas actividades.

--- app/src/main/java/com/mestabn/myapplication/ut3/alertExercise/presentation/descriptionalerts/DescriptionAlertActivity.kt
+++ app/src/main/java/com/mestabn/myapplication/ut3/alertExercise/presentation/descriptionalerts/DescriptionAlertActivity.kt
@@ -6,9 +6,14 @@ import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
+import com.mestabn.myapplication.R
 import com.mestabn.myapplication.databinding.ActivityDescriptionAlertBinding
 import com.mestabn.myapplication.ut3.alertExercise.app.MockApiAlerts
 import com.mestabn.myapplication.ut3.alertExercise.app.RetrofitApiAlerts
@@ -33,15 +38,25 @@ class DescriptionAlertActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupView()
-        loadAlert()
     }
 
     private fun setupView() {
+        setupViewBinding()
+        setupViewToolbar()
+        loadAlert()
+    }
+
+    private fun setupViewBinding() {
         setContentView(bind.root)
     }
 
-    private fun loadAlert() = Thread {
+    private fun setupViewToolbar() {
+        setSupportActionBar(bind.viewCustomToolbar.toolbar)
+        supportActionBar?.title = "Información"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 
+    private fun loadAlert() = Thread {
         val alert = viewModelDescriptionAlert.getAlert(getAlertId())
         runOnUiThread {
             if (alert != null) {
@@ -57,6 +72,49 @@ class DescriptionAlertActivity : AppCompatActivity() {
 
     private fun getAlertId(): String {
         return intent.extras!!.getString(KEY_ALERT_ID, "0")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add -> {
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.action_share -> {
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, "SHARE")
+                    type = "text/plain"
+                }
+                startActivity(Intent.createChooser(sendIntent, ""))
+                true
+
+            }
+
+            R.id.action_settings -> {
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.action_about -> {
+                Toast.makeText(this, "Versión de app: 0.0.1", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 
     companion object {
--- app/src/main/java/com/mestabn/myapplication/ut3/alertExercise/presentation/listalerts/AlertActivity.kt
+++ app/src/main/java/com/mestabn/myapplication/ut3/alertExercise/presentation/listalerts/AlertActivity.kt
@@ -1,10 +1,15 @@
 package com.mestabn.myapplication.ut3.alertExercise.presentation.listalerts
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.mestabn.myapplication.R
 import com.mestabn.myapplication.databinding.ActivityAlertBinding
 import com.mestabn.myapplication.databinding.ActivityDescriptionAlertBinding
 import com.mestabn.myapplication.ut3.alertExercise.app.MockApiAlerts
@@ -25,31 +30,38 @@ class AlertActivity : AppCompatActivity() {
         )
     )
 
-
-
-
-
     private val alertAdapter = AlertsAdapter()
-
 
     private val bind: ActivityAlertBinding by lazy {
         ActivityAlertBinding.inflate(layoutInflater)
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(bind.root)
-        getAlerts()
         setupView()
     }
 
 
-
     private fun setupView() {
+        setupViewBinding()
+        setupViewRecycler()
+        setupViewToolbar()
+        getAlerts()
+    }
+
+    private fun setupViewBinding() {
+        setContentView(bind.root)
+    }
+
+    private fun setupViewRecycler() {
         bind.listAlerts.adapter = alertAdapter
         bind.listAlerts.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    }
+
+    private fun setupViewToolbar() {
+        setSupportActionBar(bind.viewCustomToolbar.toolbar)
+        supportActionBar?.title = "Avisos"
     }
 
     private fun getAlerts() {
@@ -61,7 +73,51 @@ class AlertActivity : AppCompatActivity() {
         }.start()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add -> {
+
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.action_share -> {
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, "SHARE")
+                    type = "text/plain"
+                }
+                startActivity(Intent.createChooser(sendIntent, ""))
+                true
+
+            }
+
+            R.id.action_settings -> {
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.action_about -> {
+                Toast.makeText(this, "Versión de app: 0.0.1", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+    }
 
 
 }
+
+
