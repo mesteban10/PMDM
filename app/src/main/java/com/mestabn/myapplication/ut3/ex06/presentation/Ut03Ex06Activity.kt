0d86a8877dd623bf39d0e292d90e25f4e5b56fb7
mesteban10 <mariaesteban1997@gmail.com>
2021-11-25 18:19:31 +0100

    Implementacion de toolbar, listado de formularios.

--- app/src/main/java/com/mestabn/myapplication/ut3/ex06/presentation/Ut03Ex06Activity.kt
+++ app/src/main/java/com/mestabn/myapplication/ut3/ex06/presentation/Ut03Ex06Activity.kt
@@ -1,22 +1,101 @@
 package com.mestabn.myapplication.ut3.ex06.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.mestabn.myapplication.R
+import com.mestabn.myapplication.databinding.ActivityUt03Ex06Binding
 
 class Ut03Ex06Activity : AppCompatActivity() {
+
+    private val binding: ActivityUt03Ex06Binding by lazy {
+        ActivityUt03Ex06Binding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ut03_ex06)
+        setupView()
+
+    }
+
+    fun setupView(){
+        setupViewBinding()
+        setupFragment()
+    }
+
+
+    fun setupViewBinding() {
+        setContentView(binding.root)
+
     }
 
 
 
+
+    private fun addFragment(layoutId: Int, fragment: Fragment, tag: String) {
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.add(layoutId, fragment, tag)
+        fragmentTransition.commit()
+    }
 
     private fun replaceFragment(layoutId: Int, fragment: Fragment) {
         val fragmentTransition = supportFragmentManager.beginTransaction()
         fragmentTransition.replace(layoutId, fragment)
-        fragmentTransition.commit() //aplica los cambios
+        fragmentTransition.commit()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    private fun setupFragment(){
+        setupViewToolbarForm()
+        addFragment(binding.form.id, Ut03Ex06FormFragment.createInstance(), "1")
+    }
+
+    private fun setupViewToolbarForm() {
+        setSupportActionBar(binding.viewCustomToolbar.toolbar)
+        supportActionBar?.title = "Form"
+    }
+
+    private fun setupViewToolbarList() {
+        setSupportActionBar(binding.viewCustomToolbar.toolbar)
+        supportActionBar?.title = "Listado"
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_change_fragment -> {
+                val fragment = supportFragmentManager.findFragmentByTag("1")
+                when(fragment){
+                   null ->  replaceFragment(binding.containerFragment.id, Ut03Ex06ListFragment.createInstance())
+
+                    else -> replaceFragment(binding.containerFragment.id, Ut03Ex06FormFragment.createInstance())
+                }
+
+
+                true
+            }
+
+
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
+
+    }
+
 }
+
--- app/src/main/java/com/mestabn/myapplication/ut3/ex06/presentation/Ut03Ex06ListFragment.kt
+++ app/src/main/java/com/mestabn/myapplication/ut3/ex06/presentation/Ut03Ex06ListFragment.kt
@@ -1,4 +1,28 @@
 package com.mestabn.myapplication.ut3.ex06.presentation
 
-class Ut03Ex06ListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mestabn.myapplication.databinding.FragmentUt03Ex06ListBinding
+
+class Ut03Ex06ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentUt03Ex06ListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentUt03Ex06ListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        val TAG: String = Ut03Ex06FormFragment::class.java.simpleName
+        fun createInstance() = Ut03Ex06ListFragment()
+    }
+
 }
