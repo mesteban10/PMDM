92ce75411ad15ef477170bf9a3bb75a3bb94e868
mesteban10 <mariaesteban1997@gmail.com>
2021-11-29 10:38:53 +0100

    Refactorización de código. Se implementan corroutinas y LiveData

--- app/src/main/java/com/mestabn/myapplication/ut3/alertExercise/presentation/AlertViewState.kt
+++ app/src/main/java/com/mestabn/myapplication/ut3/alertExercise/presentation/AlertViewState.kt
@@ -1,5 +1,5 @@
 package com.mestabn.myapplication.ut3.alertExercise.presentation
 
-import java.util.*
+
 
 data class AlertViewState (var id: String, val title: String, val date: String, val body : String)
